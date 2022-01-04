ed4c64d83176d13be11d81b68c2de319a836a5f3
Taishi Kusunose <taishi.kusunose@gmail.com>
2022-01-04 18:40:40 +0900

    Initial Commit

---
+++ EsaNotes/EsaNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsaNotesApp.swift
+//  EsaNotes
+//
+//  Created by Taishi Kusunose on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct EsaNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
