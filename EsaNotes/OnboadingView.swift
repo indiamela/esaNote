bbcb379f309363ad5f1b8dc575412feedd661338
Taishi Kusunose <taishi.kusunose@gmail.com>
2022-01-10 11:49:31 +0900

    fix-OnboadingView

--- EsaNotes/OnboadingView.swift
+++ EsaNotes/OnboadingView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct OnboadingView: View {
+    @StateObject var viewModel = SignInViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            viewModel.signIn()
+        } label: {
+            Text("Log in")
+        }
     }
 }
 
