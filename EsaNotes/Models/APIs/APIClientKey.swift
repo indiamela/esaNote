7022638c6a4235a2acdaf9d72c9c5faeb2e52764
Taishi Kusunose <taishi.kusunose@gmail.com>
2022-01-10 12:34:09 +0900

    delete-APIClientKey.swift

--- EsaNotes/Models/APIs/APIClientKey.swift
+++ EsaNotes/Models/APIs/APIClientKey.swift
@@ -1,8 +1,12 @@
 //
-//  APIKey.swift
+//  APIClientKey.swift
 //  EsaNotes
 //
-//  Created by Taishi Kusunose on 2022/01/10.
 //
 
 import Foundation
+
+struct APIClientKey {
+    static let clientID = "nNXi0McgqZMMvHLM07g1OV7B_tf2ocurR2uMuQarbZ4"
+    static let clientSecret = "2BXSRlfY9UpJMYrWLkwJosCwAIWAV7E0pxyd9bfoRpk"
+}
