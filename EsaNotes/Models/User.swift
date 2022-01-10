03b4f7c417614a3102dd1f18eb96ed49b205dbf0
Taishi Kusunose <taishi.kusunose@gmail.com>
2022-01-10 11:47:18 +0900

    add-User.swift

---
+++ EsaNotes/Models/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  EsaNotes
+//
+//  Created by Taishi Kusunose on 2022/01/06.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct User: Codable {
+    let id: Int
+    let name, screenName: String?
+    let createdAt, updatedAt: String?
+    let icon: String?
+    let email: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case screenName = "screen_name"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+        case icon, email
+    }
+}
+
+// MARK: - Team
+struct Team: Codable {
+    let name, privacy, teamDescription: String
+    let icon: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case name, privacy
+        case teamDescription = "description"
+        case icon, url
+    }
+}
