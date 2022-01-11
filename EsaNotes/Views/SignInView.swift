c045fde54d52f19b44d3b0fd6379b13ebc3a0ab6
Taishi Kusunose <taishi.kusunose@gmail.com>
2022-01-11 14:27:22 +0900

    add-logos&Images

--- EsaNotes/Views/SignInView.swift
+++ EsaNotes/Views/SignInView.swift
@@ -11,15 +11,28 @@ struct SignInView: View {
     @Environment(\.dismiss) var dismiss
     @StateObject var viewModel = SignInViewModel()
     var body: some View {
-        Button {
-            viewModel.signIn()
-        } label: {
-            Text("Log in")
-        }
-        .onChange(of: viewModel.isSuccess) { success in
-            if success {
-                dismiss()
-            }
+        VStack{
+            Image("esaNote")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300)
+            Button(action: {
+                viewModel.signIn()
+            }, label: {
+                HStack{
+                    MyImage.toriGray
+                        .resizable()
+                        .scaledToFit()
+                    Text("Sign in with esa.io")
+                }
+                .frame(height: 60)
+                .frame(maxWidth: .infinity)
+                .background(MyColor.esaGreen)
+                .cornerRadius(6)
+                .font(.system(size: 25, weight: .medium, design: .default))
+                .padding()
+            })
+                .accentColor(Color.black)
         }
     }
 }
